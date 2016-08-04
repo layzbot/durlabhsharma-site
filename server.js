2ef10b364fd8a331ac20f453cd8577a11a6f6b3f
Savvion <Savvion@DFDEL000174.Dataflow.biz>
2016-08-04 21:03:41 +0530

    Deleted Duplicate Server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-var app = require('../app');
-
-app.set('port', (process.env.PORT || 5000));
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-
