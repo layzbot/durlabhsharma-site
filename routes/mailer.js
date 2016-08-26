e8d1f456f34c05b49cb151dd91d6c2c2d415b4e3
Durlabh Sharma <sharma.durlabh.93@gmail.com>
2016-08-27 02:30:35 +0530

    Mailer Working With Mailgun Config

--- routes/mailer.js
+++ routes/mailer.js
@@ -1,21 +1,32 @@
 var nodemailer = require("nodemailer");
-var express = require('express');
+var express = require('express');	
+
 var router = express.Router();
 
+//Your sending email address
+var from_who = 'sharma.durlabh.93@gmail.com';
+
 var transporter = nodemailer.createTransport("SMTP",{
-    service: "Gmail",
+    service: "Mailgun",
+/*	
+	//Auth for Google
     auth: {
         user: "sharma.durlabh.93@gmail.com",
         pass: "pxgjleunlherbmmt"
-    }
+    } */ 
+	
+	 auth: {
+        user: "postmaster@appd4cb8d5cc4c34ae893992007dd15cf85.mailgun.org",
+        pass: "9f8b1fcba8bf4d5bbdb749e473c8f57f"
+    } 
 });
 
 var mailOptions={
   /* to : req.query.to,
    subject : req.query.subject,
    text : req.query.text*/
-
-    to : "durlabh@dataflowgroup.com",
+   from:from_who,
+   to : from_who,
    subject : "Node Mail Test",
    text : "Node Test Mail Successful"
 }
@@ -30,7 +41,7 @@ router.get('/send', function(request, res, next){
             console.log("Message sent: " + response.message);
         	res.end("sent");
         }
-	});
+	}); 
 });
 
 module.exports=router;
