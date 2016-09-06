d0eb8eefd8a04e481090e081d7316c815e31408d
Durlabh Sharma <sharma.durlabh.93@gmail.com>
2016-09-06 18:59:37 +0530

    Seperated Database part

--- routes/mailer.js
+++ routes/mailer.js
@@ -1,12 +1,14 @@
 var nodemailer = require('nodemailer');
 var express = require('express');	
-var mongoose = require('mongoose');
+
+var database = require('../middleware/database');
 
 var router = express.Router();
 
 //Your sending email address
 var to = 'sharma.durlabh.93@gmail.com';
 var Email = mongoose.model('Email', {from: String, subject: String, message: String});
+
 var transporter = nodemailer.createTransport("SMTP",{
     service: "Mailgun",
 /*	
@@ -21,19 +23,6 @@ var transporter = nodemailer.createTransport("SMTP",{
         pass: "9f8b1fcba8bf4d5bbdb749e473c8f57f"
     } 
 });
-
-var uristring =
-    process.env.MONGOLAB_URI ||
-    process.env.MONGODB_URI ||
- 	'mongodb://heroku_gm7nrsbq:5t1o96gfle2b62hkk720idnti7@ds019856.mlab.com:19856/heroku_gm7nrsbq'; 
-	
-mongoose.connect(uristring, function (err, res) {
-      if (err) {
-      console.log ('ERROR connecting to: ' + uristring + '. ' + err);
-      } else {
-      console.log ('Succeeded connected to: ' + uristring);
-      }
-    });
 
 router.post('/send', function(req, res, next){
 	var mailOptions={
