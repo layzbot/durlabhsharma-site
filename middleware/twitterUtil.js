0105d1f10f26466d2173358593430a21735b2951
Durlabh Sharma <sharma.durlabh.93@gmail.com>
2017-03-29 15:14:50 +0530

    Twitter Api in Action

--- middleware/twitterUtil.js
+++ middleware/twitterUtil.js
@@ -1,10 +1,10 @@
 var Twitter = require('twitter');
 
 var client = new Twitter({
-  consumer_key: 'TQldVR9qAh8LIHthIp0DWpi9V',
-  consumer_secret: 'ljYvHKJMwsB9luqBCHje6iDiK8PcV4s30xGMXWGArAl0JCQSDX',
-  access_token_key: '787595854827839488-0pRvLPumxGwZgZRc5WrhkI9Sfjvx0X3',
-  access_token_secret: 'HJME3QS2GB8QSYQta8p0Y1jvkaLt7f2QhJnKac3TV6Zkd'
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
 
 module.exports=client;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var twitter = require('../middleware/twitterUtil');
 
 router.get('/', function(request, response, next) {
-	response.render('pages/index');
+	var finalTweets;
+	var tweets = getTweets(function(finalTweets) {
+		response.render('pages/index', {tweets:finalTweets});
+	});	
 });
 
 router.get('/angular', function(request, response, next) {
 	response.render('pages/angulardemo');
 });
+
+function getTweets(fn) {
+	
+	twitter.get('statuses/user_timeline', function(error, tweets, response) {
+	  if(error) throw error;
+	  fn(tweets);
+	});
+}
 
 module.exports=router;
